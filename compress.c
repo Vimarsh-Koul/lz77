a28c26d9ff5a0030f630e264df6e072dd693f75f
Shreeram Agarwal <sryagarwal@gmail.com>
2021-05-15 15:56:43 +0530

    Add files via upload

---
+++ compress.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+ 
+void compress(int argc, char *argv[] ){ 
+ 
+    unsigned char *prefix; 
+    unsigned char *window;  
+    int d,l;
+    char c;  
+ 
+    if(argv[1] != 0){
+        perror("Input not given");
+        return -1;
+}
+ 
+    // longest prefix of input that begins in window    
+    prefix = // remove len(argv[1])-window
+ 
+    #if PREFIX 
+        d := distance to start of prefix;
+        l := length of prefix;
+        c := char following prefix in input;
+    }
+    else 
+        d := 0;
+        l := 0;
+        c := first char of input;
+    
+     #endif
+        output (d, l, c);
+ 
+ 
+ 
+}
+ 
+ 
+Unsigned char pop(input, len,window){
+//poping function);
+}
+ 
+Unsigned char pop(input, len, window){
+//poping function);
+}
+ 
+ 
+int main(){
+ 
+ 
+	while(){
+ 
+ 
+    // discard l + 1 chars from front of window
+ 
+ 
+ 
+    //s := pop l + 1 chars from front of input
+ 
+    //append s to back of window
+ 
+ 
+//repeat
+    
+}
+}
+ 
+
